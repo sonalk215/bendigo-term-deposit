d60ee7e0cf76475686d07525b92311cadb85c3cb
SonalKhare <sonalkhare.sk215@gmail.com>
2024-09-10 07:33:24 +1000

    css changes

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -8,10 +8,6 @@ const Select = ({ classes, onChangeHandler, value, id}) => {
       value={value}
       id={id}
     >
-      <option value="">None</option>
-      <option value="Monthly">Monthly</option>
-      <option value="Quarterly">Quarterly</option>
-      <option value="Annually">Annually</option>
       <option value="At Maturity">At Maturity</option>
     </select>
   );
