63f31e79c9ce5c5810b5c58131891d7991f9e5d5
SonalKhare <sonalkhare.sk215@gmail.com>
2024-09-10 10:00:51 +1000

    css changes

--- src/components/TermDeposit/TermDeposit.jsx
+++ src/components/TermDeposit/TermDeposit.jsx
@@ -47,90 +47,93 @@ const TermDeposit = () => {
   };
 
   return (
-    <form className="termDepositForm" onSubmit={(e) => e.preventDefault()}>
-      <p>
-        <Label
-          classes="labelText"
-          labelFor="startDepositAmount"
-          labelText="Start deposit Amount"
-        />
-        <span className="userValue">{inputValues.startDepositAmount}</span>
-        <input
-          className="rangeInput startDepositAmount"
-          id="startDepositAmount"
-          type="range"
-          min="1000"
-          max="1500000"
-          defaultValue="10000"
-          onChange={inputChangeHandler}
-          onBlur={submitButtonHandler}
-        />
-      </p>
-      <p>
-        <Label
-          classes="labelText"
-          labelFor="interestRate"
-          labelText="Interest Rate"
-        />
-        <span className="userValue"></span>
-        <input
-          className="enterValueField"
-          id="interestRate"
-          type="number"
-          onChange={inputChangeHandler}
-          onBlur={submitButtonHandler}
-          step="0.10"
-          min="0.00"
-          value={inputValues.interestRate}
-        />
-      </p>
-      <p>
-        <Label
-          classes="labelText"
-          labelFor="investmentTerm"
-          labelText="Investment Term"
-        />
-        <span className="userValue">{inputValues.investmentTerm}</span>
-        <input
-          className="rangeInput"
-          id="investmentTerm"
-          type="range"
-          min="3"
-          max="60"
-          defaultValue="0"
-          onChange={investmentTermChangeHandler}
-          onBlur={submitButtonHandler}
-        />
-      </p>
-      <p>
-        <Label
-          classes="labelText"
-          labelFor="interestPaid"
-          labelText="Interest Paid"
-        />
-        <span className="userValue"></span>
-        <Select
-          classes="enterValueField"
-          id="interestPaid"
-          onChangeHandler={inputChangeHandler}
-          value={inputValues.interestPaid}
-        />
-      </p>
-      <button
-        type="submit"
-        className="submitButton"
-        onClick={submitButtonHandler}
-      >
-        Show Final Amount
-      </button>
-
-      {finalBalance === 0 ? null : (
-        <p className="finalStatement">
-          The final balance is $
-          <span className="finalBalance">{finalBalance}</span>
+    <div className="termDeposit">
+      <h1>Calculate Term Deposit</h1>
+      <form className="termDepositForm" onSubmit={(e) => e.preventDefault()}>
+        <p>
+          <Label
+            classes="labelText"
+            labelFor="startDepositAmount"
+            labelText="Start deposit Amount"
+          />
+          <span className="userValue">{inputValues.startDepositAmount}</span>
+          <input
+            className="rangeInput startDepositAmount"
+            id="startDepositAmount"
+            type="range"
+            min="1000"
+            max="1500000"
+            defaultValue="10000"
+            onChange={inputChangeHandler}
+            onBlur={submitButtonHandler}
+          />
         </p>
-      )}
-    </form>
+        <p>
+          <Label
+            classes="labelText"
+            labelFor="interestRate"
+            labelText="Interest Rate"
+          />
+          <span className="userValue"></span>
+          <input
+            className="enterValueField"
+            id="interestRate"
+            type="number"
+            onChange={inputChangeHandler}
+            onBlur={submitButtonHandler}
+            step="0.10"
+            min="0.00"
+            value={inputValues.interestRate}
+          />
+        </p>
+        <p>
+          <Label
+            classes="labelText"
+            labelFor="investmentTerm"
+            labelText="Investment Term"
+          />
+          <span className="userValue">{inputValues.investmentTerm}</span>
+          <input
+            className="rangeInput"
+            id="investmentTerm"
+            type="range"
+            min="3"
+            max="60"
+            defaultValue="0"
+            onChange={investmentTermChangeHandler}
+            onBlur={submitButtonHandler}
+          />
+        </p>
+        <p>
+          <Label
+            classes="labelText"
+            labelFor="interestPaid"
+            labelText="Interest Paid"
+          />
+          <span className="userValue"></span>
+          <Select
+            classes="enterValueField"
+            id="interestPaid"
+            onChangeHandler={inputChangeHandler}
+            value={inputValues.interestPaid}
+          />
+        </p>
+        <button
+          type="submit"
+          className="submitButton"
+          onClick={submitButtonHandler}
+        >
+          Show Final Amount
+        </button>
+
+        {finalBalance === 0 ? null : (
+          <p className="finalStatement">
+            The final balance is $
+            <span className="finalBalance">{finalBalance}</span>
+          </p>
+        )}
+      </form>
+    </div>
   );
 };
 
